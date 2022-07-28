e94f928770e9c564913cb539ee951eeddab01c3d
damiru2 <chalkarov11@mail.ru>
2022-07-28 20:55:59 +0500

    created UIActivityIndiacatorView by code

--- UIActivityIndicatorView/ViewController.swift
+++ UIActivityIndicatorView/ViewController.swift
@@ -8,10 +8,33 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var activityIndicatorView = UIActivityIndicatorView(style: .large)
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.addSubview(activityIndicatorView)
+        activityIndicatorView.center = view.center
+        
+        activityIndicatorView.color = .green
+        activityIndicatorView.startAnimating()
+        activityIndicatorView.hidesWhenStopped = true
+        
+        
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 2) {
+            self.activityIndicatorView.stopAnimating()
+            
+            DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 2) {
+                self.activityIndicatorView.startAnimating()
+            }
+        }
     }
 
 
